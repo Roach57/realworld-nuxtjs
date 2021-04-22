d62ef067f21f6bbd7e3ce85d3fe48b38787ce475
吴强 <wuqiang@datahunter.cn>
2021-04-23 07:00:18 +0800

    feat: 1.1 项目初始化-创建项目

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # realworld-nuxtjs
+
+## 参考链接
+[官方模板](https://github.com/gothinkster/realworld-starter-kit/blob/master/FRONTEND_INSTRUCTIONS.md)
+[国内 CDN 代理](https://jsdelivr.com)
---
+++ tmp/layout/index.vue
@@ -0,0 +1,63 @@
+<!--  -->
+<template>
+<div class=''>
+  <!-- 顶部导航栏 -->
+  <nav class="navbar navbar-light">
+
+  </nav>
+  <!-- /顶部导航栏 -->
+
+  <!-- 子路由 -->
+  <nuxt-child/>
+  <!-- /子路由 -->
+
+  <!-- 底部 -->
+        <div class="container">
+        <a href="/" class="logo-font">conduit</a>
+        <span class="attribution">
+          An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+        </span>
+      </div>
+  <!-- /底部 -->
+
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'LayoutIndex',
+  components: {},
+  props: {},
+  data() {
+  //这里存放数据
+    return {
+
+  };
+  },
+  //监听属性 类似于 data 概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //生命周期 - 创建完成（可以访问当前 this 实例）
+  created() {
+
+  },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {
+
+  },
+  //方法集合
+  methods: {
+
+  },
+  //beforeCreate() {}, //生命周期 - 创建之前
+  //beforeMount() {}, //生命周期 - 挂载之前
+  //beforeUpdate() {}, //生命周期 - 更新之前
+  //updated() {}, //生命周期 - 更新之后
+  //beforeDestroy() {}, //生命周期 - 销毁之前
+  //destroyed() {}, //生命周期 - 销毁完成
+  //activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+<style scoped></style>
