98f93ab8efae16febf88c0f82dbfac45b6b982c8
吴强 <wuqiang@datahunter.cn>
2021-04-25 20:34:32 +0800

    feat: 4.4 文章详情-设置页面 meta 优化 SEO

---
+++ pages/article/components/article-meta.vue
@@ -0,0 +1,94 @@
+<!--  -->
+<template>
+  <div class="article-meta">
+    <nuxt-link
+      :to="{
+        name: 'profile',
+        params: {
+          username: article.author.username
+        }
+      }"
+    >
+      <img :src="article.author.image" />
+    </nuxt-link>
+    <div class="info">
+      <nuxt-link
+        class="author"
+        :to="{
+          name: 'profile',
+          params: {
+            username: article.author.username
+          }
+        }"
+      >
+        {{ article.author.username }}
+      </nuxt-link>
+      <span class="date">{{ article.createAt | date('MMM DD, YYYY') }}</span>
+    </div>
+    <button
+      class="btn btn-sm btn-outline-secondary"
+      :class="{
+        active: article.author.following
+      }"
+    >
+      <i class="ion-plus-round"></i>
+      &nbsp;
+      Follow Eric Simons <span class="counter">(10)</span>
+    </button>
+    &nbsp;&nbsp;
+    <button
+      class="btn btn-sm btn-outline-primary"
+      :class="{
+        active: article.favorited
+      }"
+    >
+      <i class="ion-heart"></i>
+      &nbsp;
+      Favorite Post <span class="counter">(29)</span>
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ArticleMeta',
+  components: {},
+  props: {
+    article: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+  //这里存放数据
+    return {
+
+    };
+  },
+  //监听属性 类似于 data 概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //生命周期 - 创建完成（可以访问当前 this 实例）
+  created() {
+
+  },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {
+
+  },
+  //方法集合
+  methods: {
+
+  },
+  //beforeCreate() {}, //生命周期 - 创建之前
+  //beforeMount() {}, //生命周期 - 挂载之前
+  //beforeUpdate() {}, //生命周期 - 更新之前
+  //updated() {}, //生命周期 - 更新之后
+  //beforeDestroy() {}, //生命周期 - 销毁之前
+  //destroyed() {}, //生命周期 - 销毁完成
+  //activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+<style scoped></style>
