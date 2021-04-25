c98d8cf70743dcbe7e5b07ea4ea8dbea6150dba5
吴强 <wuqiang@datahunter.cn>
2021-04-25 21:11:48 +0800

    feat: 4.6 文章详情-通过客户端渲染展示评论列表

---
+++ pages/article/components/article-components.vue
@@ -0,0 +1,99 @@
+<!--  -->
+<template>
+  <div class=''>
+    <form class="card comment-form">
+      <div class="card-block">
+        <textarea class="form-control" placeholder="Write a comment..." rows="3"></textarea>
+      </div>
+      <div class="card-footer">
+        <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
+        <button class="btn btn-sm btn-primary">
+        Post Comment
+        </button>
+      </div>
+    </form>
+
+    <div
+      class="card"
+      v-for="comment in comments"
+      :key="comment.id"
+    >
+      <div class="card-block">
+        <p class="card-text">{{ comment.body }}</p>
+      </div>
+      <div class="card-footer">
+        <nuxt-link
+          class="comment-author"
+          :to="{
+            name: 'profile',
+            params: {
+              username: comment.author.username
+            }
+          }"
+        >
+          <img :src="comment.author.image" class="comment-author-img" />
+        </nuxt-link>
+        &nbsp;
+        <nuxt-link
+          class="comment-author"
+          :to="{
+            name: 'profile',
+            params: {
+              username: comment.author.username
+            }
+          }"
+        >
+          {{ comment.author.username }}
+        </nuxt-link>
+        <span class="date-posted">{{ comment.createAt | date('MMM DD, YYYY') }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getComments } from '@/api/article'
+
+export default {
+  name: 'ArticleComments',
+  components: {},
+  props: {
+    article: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+  //这里存放数据
+    return {
+      comments: [] //文章列表
+    };
+  },
+  //监听属性 类似于 data 概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //生命周期 - 创建完成（可以访问当前 this 实例）
+  created() {
+
+  },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  async mounted() {
+    const { data } = await getComments(this.article.slug)
+    // console.log(data)
+    this.comments = data.comments
+  },
+  //方法集合
+  methods: {
+
+  },
+  //beforeCreate() {}, //生命周期 - 创建之前
+  //beforeMount() {}, //生命周期 - 挂载之前
+  //beforeUpdate() {}, //生命周期 - 更新之前
+  //updated() {}, //生命周期 - 更新之后
+  //beforeDestroy() {}, //生命周期 - 销毁之前
+  //destroyed() {}, //生命周期 - 销毁完成
+  //activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+<style scoped></style>
--- pages/article/index.vue
+++ pages/article/index.vue
@@ -24,50 +24,8 @@
 
         <div class="col-xs-12 col-md-8 offset-md-2">
 
-          <form class="card comment-form">
-            <div class="card-block">
-              <textarea class="form-control" placeholder="Write a comment..." rows="3"></textarea>
-            </div>
-            <div class="card-footer">
-              <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-              <button class="btn btn-sm btn-primary">
-              Post Comment
-              </button>
-            </div>
-          </form>
+        <article-comments :article="article" />
 
-          <div class="card">
-            <div class="card-block">
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-              <a href="" class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-              </a>
-              &nbsp;
-              <a href="" class="comment-author">Jacob Schmidt</a>
-              <span class="date-posted">Dec 29th</span>
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="card-block">
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-              <a href="" class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-              </a>
-              &nbsp;
-              <a href="" class="comment-author">Jacob Schmidt</a>
-              <span class="date-posted">Dec 29th</span>
-              <span class="mod-options">
-                <i class="ion-edit"></i>
-                <i class="ion-trash-a"></i>
-              </span>
-            </div>
-          </div>
-          
         </div>
 
       </div>
@@ -81,11 +39,13 @@
 import { getArticle } from '@/api/article'
 import MarkdownIt from 'markdown-it'
 import ArticleMeta from  './components/article-meta'
+import ArticleComments from  './components/article-components'
 
 export default {
   name: 'ArticleIndex',
   components: {
-    ArticleMeta
+    ArticleMeta,
+    ArticleComments
   },
   props: {},
   data() {
